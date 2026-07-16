b00b0b691b82b065e164b2e138cfb658d783de3b
Aleksei Tretiakov <tellrax1990gmail.com>
2026-07-16 17:36:18 +0700

    chore:save project before Android prebuild

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,59 +1,79 @@
 import {View, Text} from 'react-native';
+import {useState, useEffect} from 'react';
+import {loadWorkouts} from '../../storage/workoutStorage';
+import {Workout} from '../../types/workout';
 
 export default function CalendarScreen() {
+    const [workouts, setWorkouts] = 
+    useState<Workout[]>([]);
+
+      const [selectedWorkout, setSelectedWorkout] = 
+    useState<Workout | null>(null);
+    
     const days = Array.from(
         {length: 31},
          (_, index) => index+1);
-  return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#206126',
-        paddingTop: 80,
-        paddingHorizontal: 20,
-              }}
-    >
-      <Text
-        style={{
-            color: 'white',
+
+
+    useEffect(() => {
+        async function fetchWorkouts() {
+            const data = await loadWorkouts();
+            setWorkouts(data || []);
+        }
+        fetchWorkouts();
+    }, []);
+
+    const workoutDates = workouts.map((workout) =>
+         workout.date
+    .split('T')[0]);
+
+    return (
+      <View style={{ flex: 1, padding: 20 }}>
+        <Text
+          style={{
+            color: 'green',
             fontSize: 32,
             fontWeight: 'bold',
-        marginBottom: 20,
-        }}
-      >
-        July 2026
+            marginBottom: 20,
+            marginTop: 20,
+          }}
+        >
+          July 2026
         </Text>
         <View
-        style={{
+          style={{
             flexDirection: 'row',
-            flexWrap: 'wrap',   
+            flexWrap: 'wrap',
             gap: 10,
-        }}
+          }}
         >
-        {days.map((day) => (
-            <View
+          {days.map((day) => {
+            const dateString = `2026-07-${day.toString().padStart(2, '0')}`;
+            const hasWorkout = workoutDates.includes(dateString);
+            return (
+              <View
                 key={day}
                 style={{
-                    width: 40,
-                    height: 40,
-                    backgroundColor: '#222',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    borderRadius: 5,
+                  width: 40,
+                  height: 40,
+                  backgroundColor: hasWorkout ? '#4CAF50' : '#222',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: 5,
                 }}
-            >
+              >
                 <Text
-                    style={{
-                        color: 'white',
-                        fontSize: 16,
-                    }}
+                  style={{
+                    color: 'white',
+                    fontSize: 16,
+                  }}
                 >
-                    {day}
+                  {day}
                 </Text>
-            </View>
-        ))}
-        </View>  
-      
-    </View>
-  );
+              </View>
+            );
+          })}
+        </View>
+      </View>
+    );
 }
